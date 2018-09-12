001aa3eb5e4755c21255d52d689216fd77fe7d40
dpinart <dpinart@newuibcn.com>
2018-09-12 13:20:17 +0200

    fix: dist bug fixed

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -4,7 +4,7 @@ const moduleName = "@uxland/uxl-prism";
 const path = require("path");
 const nodeModules = "node_modules";
 
-gulp.task("generate-d-ts", () => {
+gulp.task("generate-d-ts", (done) => {
     require("dts-generator").default({
         name: moduleName,
         out: "./index.d.ts",
@@ -14,6 +14,6 @@ gulp.task("generate-d-ts", () => {
             console.log(params.currentModuleId);
             return params.currentModuleId === "index" ? moduleName : `${moduleName}/${params.currentModuleId}`;
         }
-    });
+    }).then(done);
 });
 gulp.task("dist", () => run("npm run dist").exec());
