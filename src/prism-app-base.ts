b2217b24e8e1a836da6496cc57241d2fcabd96aa
Alex Vizcaino <avizcaino@uxland.es>
2019-10-01 07:38:30 +0200

    fix: set view in store in PrismAppBase

--- src/prism-app-base.ts
+++ src/prism-app-base.ts
@@ -1,55 +1,57 @@
-import {LitElement} from "lit-element";
-import {BootstrapOptions} from "./bootstrapper";
-import {Redux} from "./mixins/redux";
-import {isLoggedInSelector} from "./user/selectors";
-import {appInitializedSelector} from "./app/initialized/app-initialized-selector";
-import {watch} from "@uxland/lit-redux-connect";
+import { watch } from '@uxland/lit-redux-connect';
+import { LitElement } from 'lit-element';
+import { appInitializedSelector } from './app/initialized/app-initialized-selector';
+import { BootstrapOptions } from './bootstrapper';
+import { Redux } from './mixins/redux';
+import { isLoggedInSelector } from './user/selectors';
+import { MainViewType } from './view';
+import { setView } from './view/set-view';
 export interface Settings {
-    apiUrl: string;
-    logoutUrl?: string;
-    propagateCredentials?: boolean;
-    autoLogin?: boolean;
-    autoLoginUserName?: string;
-    autoLoginPassword?: string;
-    initialDelay?: number;
-    appsBaseRoute?: string;
-    offlineEnabled?: boolean;
+  apiUrl: string;
+  logoutUrl?: string;
+  propagateCredentials?: boolean;
+  autoLogin?: boolean;
+  autoLoginUserName?: string;
+  autoLoginPassword?: string;
+  initialDelay?: number;
+  appsBaseRoute?: string;
+  offlineEnabled?: boolean;
 }
 export interface UxlPrism {
-    settings: Settings;
+  settings: Settings;
 }
 
 declare interface Window {
-    uxlPrism: UxlPrism;
+  uxlPrism: UxlPrism;
 }
 declare var uxlPrism: UxlPrism;
-export abstract class PrismAppBase extends Redux(LitElement){
-    options: BootstrapOptions = {
-        fetchUser: undefined,
-        apiUrl: uxlPrism.settings.apiUrl,
-        locales: {ca:{}},
-        language: 'ca',
-        appsBaseRoute: uxlPrism.settings.appsBaseRoute,
-        moduleBaseRoute: '/'
-    };
-    connectedCallback(){
-        super.connectedCallback();
-        this.initApp();
-    }
-    protected abstract initApp();
-    protected getPagePath(page: any): string {
-        return null;
-    }
-     private _currentView: string;
+export abstract class PrismAppBase extends Redux(LitElement) {
+  options: BootstrapOptions = {
+    fetchUser: undefined,
+    apiUrl: uxlPrism.settings.apiUrl,
+    locales: { ca: {} },
+    language: 'ca',
+    appsBaseRoute: uxlPrism.settings.appsBaseRoute,
+    moduleBaseRoute: '/'
+  };
+  connectedCallback() {
+    super.connectedCallback();
+    this.initApp();
+  }
+  protected abstract initApp();
+  protected getPagePath(page: any): string {
+    return null;
+  }
+  private _currentView: string;
 
-    @watch(isLoggedInSelector)
-    loggedIn: boolean;
-    @watch(appInitializedSelector)
-    initialized: boolean;
-    protected get currentView(): string{
-        let view = !this.initialized ? 'splash' : this.loggedIn ? 'shell' : 'login';
-        if(view !== this._currentView)
-            this._currentView = view;
-        return view;
-    }
+  @watch(isLoggedInSelector)
+  loggedIn: boolean;
+  @watch(appInitializedSelector)
+  initialized: boolean;
+  protected get currentView(): string {
+    let view: MainViewType = !this.initialized ? 'splash' : this.loggedIn ? 'shell' : 'login';
+    if (view !== this._currentView) this._currentView = view;
+    setView(view);
+    return view;
+  }
 }
