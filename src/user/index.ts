cd50571d8d8a6bc5270467f374eb992551f94ae7
danielcabiscol <danielcabiscol@gmail.com>
2020-02-10 15:17:44 +0100

    fix: added export set modules

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,5 +1,6 @@
 export * from './actions';
 export * from './fetch';
 export * from './login';
-export {reducer as userReducer, UserState, ModuleInfo, ModuleType, UserInfo} from './reducer';
+export { reducer as userReducer, UserState, ModuleInfo, ModuleType, UserInfo } from './reducer';
 export * from './selectors';
+export * from './set-modules';
