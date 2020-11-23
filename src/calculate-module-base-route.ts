927c115aa95e9875b6890daa64d9e0f40f1515ac
Alex Vizcaino <avizcaino@uxland.es>
2020-11-23 13:31:16 +0100

    fix: localize polymer dependencies

--- src/calculate-module-base-route.ts
+++ src/calculate-module-base-route.ts
@@ -1,6 +1,12 @@
-import {ModuleInfo} from "./user";
-import {appsBaseRouteSelector} from "./options";
-import {store} from "./store";
-import * as settings from '@polymer/polymer/lib/utils/settings';
-export const calculateModuleBaseRoute: (moduleInfo: ModuleInfo) => string = moduleInfo => moduleInfo.url ?
-    moduleInfo.url.substring(0, moduleInfo.url.indexOf('/main.js')) : `${settings['rootPath']}${appsBaseRouteSelector(store.getState())}${moduleInfo.folder}`;
+import { ModuleInfo } from "./user";
+import { appsBaseRouteSelector } from "./options";
+import { store } from "./store";
+import * as settings from "./polymer/settings";
+export const calculateModuleBaseRoute: (moduleInfo: ModuleInfo) => string = (
+  moduleInfo
+) =>
+  moduleInfo.url
+    ? moduleInfo.url.substring(0, moduleInfo.url.indexOf("/main.js"))
+    : `${settings["rootPath"]}${appsBaseRouteSelector(store.getState())}${
+        moduleInfo.folder
+      }`;
--- src/prism-shell-mixin.ts
+++ src/prism-shell-mixin.ts
@@ -1,13 +1,15 @@
-import {dedupingMixin} from "@polymer/polymer/lib/utils/mixin";
-import {Routing} from "./mixins";
-import {RoutingMixinConstructor} from "@uxland/uxl-routing";
-import {MixinFunction} from "@uxland/uxl-utilities";
+import { Routing } from "./mixins";
+import { RoutingMixinConstructor } from "@uxland/uxl-routing";
+import { MixinFunction } from "@uxland/uxl-utilities";
+import { dedupingMixin } from "./polymer/deduping-mixin";
 
 export type PrismShellMixinFunction = MixinFunction<RoutingMixinConstructor>;
-export const PrismShellMixin: PrismShellMixinFunction = dedupingMixin(parent => {
-   class mixin extends Routing(parent){
-       subroute: string = '/';
-   }
+export const PrismShellMixin: PrismShellMixinFunction = dedupingMixin(
+  (parent) => {
+    class mixin extends Routing(parent) {
+      subroute: string = "/";
+    }
 
-   return <any>mixin;
-});
+    return <any>mixin;
+  }
+);
