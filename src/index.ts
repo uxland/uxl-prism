85c733991ed6fb139b84840c0db2c82090b90985
Alex Vizcaino <avizcaino@uxland.es>
2020-12-01 10:44:42 +0100

    fix: export new custom function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-export * from './app';
-export * from './bootstrapper';
-export * from './calculate-module-base-route';
-export * from './constants';
-export * from './disconnect';
-export * from './mixins';
-export * from './options';
-export * from './prism-app-base';
-export * from './prism-shell-mixin';
-export { init as initRouter, router } from './router';
+export * from "./app";
+export * from "./bootstrapper";
+export * from "./calculate-module-base-route";
+export * from "./constants";
+export * from "./disconnect";
+export * from "./mixins";
+export * from "./options";
+export * from "./prism-app-base";
+export * from "./prism-shell-mixin";
+export { init as initRouter, router } from "./router";
 export {
   disconnect as storeDisconnect,
   DISCONNECT_ACTION,
@@ -15,7 +15,8 @@ export {
   PrismAppState,
   ReducerDictionary,
   resetReducers,
-  store
-} from './store';
-export * from './user';
-export { reducer as viewReducer, setView, viewSelector } from './view';
+  customResetReducers,
+  store,
+} from "./store";
+export * from "./user";
+export { reducer as viewReducer, setView, viewSelector } from "./view";
