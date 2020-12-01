8fe93e240dc7c0ffcf384fab5b4c0ce161347a5a
Alex Vizcaino <avizcaino@uxland.es>
2020-12-01 10:34:16 +0100

    fix: add customResetReducers capability

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,24 @@
-import { LocalizationState, reducer as localization } from '@uxland/uxl-localization';
-import { Action } from '@uxland/uxl-redux';
-import { reducer as routing, RoutingState } from '@uxland/uxl-routing';
-import { applyMiddleware, combineReducers, compose, createStore, Reducer, Store } from 'redux';
-import thunk from 'redux-thunk';
-import { ApplicationState, reducer as app } from './app/reducer';
-import { actionsBuilder } from './constants';
-import { OptionsState, reducer as options } from './options/reducer';
-import { reducer as user, UserState } from './user/reducer';
-import { MainViewType, reducer as view } from './view/reducer';
+import {
+  LocalizationState,
+  reducer as localization,
+} from "@uxland/uxl-localization";
+import { Action } from "@uxland/uxl-redux";
+import { reducer as routing, RoutingState } from "@uxland/uxl-routing";
+import { pickAll } from "ramda";
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  createStore,
+  Reducer,
+  Store,
+} from "redux";
+import thunk from "redux-thunk";
+import { ApplicationState, reducer as app } from "./app/reducer";
+import { actionsBuilder } from "./constants";
+import { OptionsState, reducer as options } from "./options/reducer";
+import { reducer as user, UserState } from "./user/reducer";
+import { MainViewType, reducer as view } from "./view/reducer";
 
 export interface PrismAppState {
   app: ApplicationState;
@@ -20,23 +31,30 @@ export interface PrismAppState {
 
 export type ReducerDictionary = { [key: string]: Reducer };
 let reducers: ReducerDictionary = {};
-export const DISCONNECT_ACTION = actionsBuilder('disconnect');
+export const DISCONNECT_ACTION = actionsBuilder("disconnect");
 const createReducer = (reducers: ReducerDictionary = {}) => {
-  let mainReducer = combineReducers({ ...{ localization, routing, user, view, app, options }, ...reducers });
+  let mainReducer = combineReducers({
+    ...{ localization, routing, user, view, app, options },
+    ...reducers,
+  });
   return (state: PrismAppState, action: Action) =>
     mainReducer(
       action.type === DISCONNECT_ACTION
         ? <any>{
             app: state.app,
             localization: state.localization,
-            options: state.options
+            options: state.options,
           }
         : state,
       action
     );
 };
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store: Store<PrismAppState> = createStore(createReducer(), composeEnhancers(applyMiddleware(thunk)));
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store: Store<PrismAppState> = createStore(
+  createReducer(),
+  composeEnhancers(applyMiddleware(thunk))
+);
 export const disconnect = () => {
   store.replaceReducer(createReducer());
   store.dispatch({ type: DISCONNECT_ACTION });
@@ -48,4 +66,8 @@ export const injectReducer = (name: string, reducer: Reducer<any, any>) => {
 export const resetReducers = () => {
   reducers = {};
   store.replaceReducer(createReducer());
+};
+export const customResetReducers = (keys: string[]) => {
+  reducers = pickAll(keys, reducers);
+  store.replaceReducer(createReducer(reducers));
 };
