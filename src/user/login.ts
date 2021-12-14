04d4a880fa9b23570d63add8a68e7fa79f8ba29e
Alex Vizcaino <avizcaino@uxland.es>
2021-12-14 14:29:40 +0100

    fix: add center as optional parameter in login

--- src/user/login.ts
+++ src/user/login.ts
@@ -1,21 +1,42 @@
-import { fetchClient } from '@uxland/uxl-fetch-client';
-import { store } from '../store';
-import { loginUserAction } from './actions';
-import { LoginInfo, UserInfo } from './reducer';
+import { fetchClient } from "@uxland/uxl-fetch-client";
+import { store } from "../store";
+import { loginUserAction } from "./actions";
+import { LoginInfo, UserInfo } from "./reducer";
 
-export type fetchLoginFunc = <T extends LoginInfo>(username: string, password: string) => Promise<T>;
-export type fetchUserFunc = <T extends UserInfo>(username: string, password: string) => Promise<T>;
+export type fetchLoginFunc = <T extends LoginInfo>(
+  username: string,
+  password: string,
+  center?: string
+) => Promise<T>;
+export type fetchUserFunc = <T extends UserInfo>(
+  username: string,
+  password: string,
+  center?: string
+) => Promise<T>;
 const toAuthorizationHeader = (username: string, password) => ({
-  headers: { Authorization: 'Basic ' + btoa(username + ':' + password) }
+  headers: { Authorization: "Basic " + btoa(username + ":" + password) },
 });
 export const setUserLogin = (fetch: string | fetchUserFunc) => {
   doLogin =
-    typeof fetch === 'string' ? (uname, pswrd) => fetchClient.fetch(fetch, toAuthorizationHeader(uname, pswrd)) : fetch;
+    typeof fetch === "string"
+      ? (uname, pswrd) =>
+          fetchClient.fetch(fetch, toAuthorizationHeader(uname, pswrd))
+      : fetch;
 };
 
 let doLogin: fetchUserFunc;
 
-export const login = async (username: string, password: string) => {
-  let userInfo = await store.dispatch(loginUserAction((uname, pwd) => doLogin(uname, pwd))(username, password));
+export const login = async (
+  username: string,
+  password: string,
+  center?: string
+) => {
+  let userInfo = await store.dispatch(
+    loginUserAction((uname, pwd, center) => doLogin(uname, pwd, center))(
+      username,
+      password,
+      center
+    )
+  );
   return userInfo;
 };
